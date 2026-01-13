764700032f87bba88df07087f85eddc2cf9945e2
Skamor <184520326+Skamor@users.noreply.github.com>
2026-01-13 16:14:56 +0000

    ajout d'un menu d'option et régler un problème de l'interface

--- ui.py
+++ ui.py
@@ -2,16 +2,30 @@ from grille import grille
 from piece import piece
 
 def main():
-    jeu = grille()
     while True:
-        tour_joueur_1(jeu)
-        jeu.show_game_state()
-        if jeu.check_win():
-            print("le joueur 1 a gagner")
+        while True:
+            print("que voulez vous faire?\n1. jouer\n2. quitter\nentrez un chiffre (1 ou 2)")
+            anwser = int(input())
+            if not (anwser == 1 or anwser == 2):
+                print("réponse invalide")
+            else:
+                break
+        if anwser == 1:
+            jeu = grille()
+            while True:
+                tour_joueur_1(jeu)
+                jeu.show_game_state()
+                if jeu.check_win():
+                    print("le joueur 1 a gagner")
+                    break
+                tour_joueur_2(jeu)
+                jeu.show_game_state()
+                if jeu.check_win():
+                    print("le joueur 2 a gagner")
+                    break
+        if anwser == 2:
+            print("bye bye")
             break
-        tour_joueur_2(jeu)
-        if jeu.check_win():
-            print("le joueur 2 a gagner")
 
 def tour_joueur_1(jeu):
     print("tour au joueur 1")
